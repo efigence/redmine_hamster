e9ccb55829a222fef47fd8c652e9f8b241533111
swiatkiewicz <m.swiatkiewicz91@gmail.com>
2016-02-09 16:59:15 +0100

    Update layout_hook.rb
    fix typo

--- lib/redmine_hamster/hooks/layout_hook.rb
+++ lib/redmine_hamster/hooks/layout_hook.rb
@@ -1,4 +1,4 @@
-module RedmineIssueTags
+module RedmineHamster
   module Hooks
     class LayoutHook < Redmine::Hook::ViewListener
       render_on(:view_layouts_base_html_head, :partial => 'my/current_issue', :layout => false)
